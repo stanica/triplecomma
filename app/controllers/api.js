aff7e53550744beb196f08c0b0df8003c006e2a1
Jack Song <jack@tophat.com>
2015-10-03 00:17:23 -0400

    working twitter api call

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -287,46 +287,23 @@ exports.getLastfm = function(req, res, next) {
  * Twiter API example.
  */
 exports.getTwitter = function(req, res, next) {
-  Twit = require('twit');
-
+  var Twitter = require('twitter');
+ 
   var token = _.find(req.user.tokens, { kind: 'twitter' });
-  var T = new Twit({
+  var client = new Twitter({
     consumer_key: secrets.twitter.consumerKey,
     consumer_secret: secrets.twitter.consumerSecret,
-    access_token: token.accessToken,
+    access_token_key: token.accessToken,
     access_token_secret: token.tokenSecret
   });
-  T.get('search/tweets', { q: 'nodejs since:2013-01-01', geocode: '40.71448,-74.00598,5mi', count: 10 }, function(err, reply) {
-    if (err) return next(err);
-    res.render('api/twitter', {
-      title: 'Twitter API',
-      tweets: reply.statuses
-    });
-  });
-};
-
-/**
- * POST /api/twitter
- * Post a tweet.
- */
-exports.postTwitter = function(req, res, next) {
-  req.assert('tweet', 'Tweet cannot be empty.').notEmpty();
-  var errors = req.validationErrors();
-  if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/api/twitter');
-  }
-  var token = _.find(req.user.tokens, { kind: 'twitter' });
-  var T = new Twit({
-    consumer_key: secrets.twitter.consumerKey,
-    consumer_secret: secrets.twitter.consumerSecret,
-    access_token: token.accessToken,
-    access_token_secret: token.tokenSecret
-  });
-  T.post('statuses/update', { status: req.body.tweet }, function(err, data, response) {
-    if (err) return next(err);
-    req.flash('success', { msg: 'Tweet has been posted.'});
-    res.redirect('/api/twitter');
+   
+  var params = {
+                q: 'node.js'
+              };
+  client.get('search/tweets', params, function(error, tweets, response){
+    if (!error) {
+      res.json(tweets);
+    }
   });
 };
 
